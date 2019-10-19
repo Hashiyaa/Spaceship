de6924f5676ee68dfb64a35cfc6c628a22fd175f
ruipeterpan <rpan0726@gmail.com>
2019-10-19 11:34:19 -0500

    Create UFO.js

---
+++ UFO.js
@@ -0,0 +1,17 @@
+class UFO extends FlyingObject {
+    constructor(x, y){
+        super(x, y);
+    }
+
+    setType(t) {
+        this.type = t;
+    }
+
+    getType() {
+        return this.type;
+    }
+
+    onCollisionWith(g) {
+        if 
+    }
+}
