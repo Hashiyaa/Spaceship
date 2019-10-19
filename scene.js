7198d5b742ec5e66b97a627b62850996a4cf87f4
Chen-Yuqiu <35789463+Chen-Yuqiu@users.noreply.github.com>
2019-10-19 15:51:46 -0500

    delete menu

--- scene.js
+++ scene.js
@@ -134,9 +134,15 @@ function LoadScene() {
 
 window.onload = function(){
     // Get canvas element and its context
+    let canvas = /** @type {HTMLCanvasElement} */ (document.getElementById("canvas"));
+    let ctx = canvas.getContext('2d');
+
     let MainFrame = document.getElementById("MainFrame");
+
     let StartButton = document.createElement("button");
-    StartButton.onclick = LoadScene;
+
+    StartButton.innerHTML = "START";
+    StartButton.setAttribute("onclick","LoadScene()");
     StartButton.setAttribute("id","StartButton");
     MainFrame.appendChild(StartButton);
 }
