9de7ed0068f8a13c255a929fa5e9983c6925f50f
Chen-Yuqiu <35789463+Chen-Yuqiu@users.noreply.github.com>
2019-10-19 12:33:46 -0500

    change menu.js

--- Menu.js
+++ Menu.js
@@ -3,27 +3,4 @@ window.onload = function(){
     let canvas = /** @type {HTMLCanvasElement} */ (document.getElementById("canvas"));
     let ctx = canvas.getContext('2d');
 
-    // Button position and dimensions
-    var buttonX = 80;
-    var buttonY = 80;
-    var buttonW = 60;
-    var buttonH = 30;
-
-    // Render button
-    ctx.fillStyle = 'red';
-    ctx.fillRect(buttonX, buttonY, buttonW, buttonH);
-
-    // Add event listener to canvas element
-    canvas.addEventListener('click', function(event) {
-    // Control that click event occurred within position of button
-    // NOTE: This assumes canvas is positioned at top left corner 
-    if (
-        event.x > buttonX && 
-        event.x < buttonX + buttonW &&
-        event.y > buttonY && 
-        event.y < buttonY + buttonH
-    ){
-        // Executes if button was clicked!
-      }
-    });
 }
