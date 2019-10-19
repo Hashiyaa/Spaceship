ddceee154a639db037e347220f8e22f8af94512e
Chen-Yuqiu <35789463+Chen-Yuqiu@users.noreply.github.com>
2019-10-19 15:50:03 -0500

    1

--- Menu.js
+++ Menu.js
@@ -7,11 +7,8 @@ window.onload = function(){
     let MainFrame = document.getElementById("MainFrame");
 
     let StartButton = document.createElement("button");
-<<<<<<< HEAD
 
     StartButton.innerHTML = "START";
-=======
->>>>>>> 2e275f49b83fa1263e3b3d9a61f703e218f99186
     StartButton.setAttribute("onclick","LoadScene()");
     StartButton.setAttribute("id","StartButton");
     MainFrame.appendChild(StartButton);
