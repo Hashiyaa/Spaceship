45fee122188163de69f6110fce9b23f1cad66773
Chen-Yuqiu <35789463+Chen-Yuqiu@users.noreply.github.com>
2019-10-19 13:17:54 -0500

    Update Menu.js

--- Menu.js
+++ Menu.js
@@ -3,4 +3,21 @@ window.onload = function(){
     let canvas = /** @type {HTMLCanvasElement} */ (document.getElementById("canvas"));
     let ctx = canvas.getContext('2d');
 
+    let MainFrame = document.getElementById("Mainframe");
+
+    let StartButton = document.createElement("button");
+    
+    StartButton.style.backgroundColor = grey;
+
+    let StartButtonStyle = 
+    "background-color: grey;\
+        left: 1150px;\
+        position: absolute;\
+        width: 280px;\
+        height: 110px;\
+        font-size: 40px;\
+        top: 220px;\
+    ";
+
+    MainFrame.appendChild(StartButton);
 }
