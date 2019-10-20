ced30e19081e5833f58b8b8d1bf06f26eb50c1e7
Chen-Yuqiu <35789463+Chen-Yuqiu@users.noreply.github.com>
2019-10-19 20:57:04 -0500

    Update scene.js

--- scene.js
+++ scene.js
@@ -39,10 +39,7 @@ function LoadScene() {
     let img = new Image(); // Create new img element
     img.src = 'images/spaceship.png'; // Set source path
 
-    function ButtonDisappear(){
-        document.getElementById("StartButton").remove();
-        document.getElementById("SettingButton").remove();
-    }
+
     ButtonDisappear();
 
     function LoadEnergy(){
@@ -214,6 +211,10 @@ function LoadScene() {
         dirX = 0;
         dirY = 0;
     }
+
+ 
+    
+
 }
 
 window.onload = function(){
@@ -236,12 +237,36 @@ window.onload = function(){
 
     main.appendChild(StartButton);
     main.appendChild(SettingButton);
+
+    
+
+}
+function ButtonDisappear(){
+    document.getElementById("StartButton").remove();
+    document.getElementById("SettingButton").remove();
 }
 
 function Settings(){
-
+    ButtonDisappear();
+    gameover();
 }
+
 function gameover(){
+    let context = canvas.getContext("2d");
     context.clearRect(0, 0, canvas.width, canvas.height);
     context.save();
+    let gameover_str = document.createElement("pre");
+    let scoretext = document.createElement("pre");
+
+    scoretext.innerHTML = "Score:"+score;
+    gameover_str.innerHTML = "GAME OVER";
+
+    scoretext.setAttribute("id","ScoreText");
+    gameover_str.setAttribute("id","GameOverStr");
+    
+    let mainframe = document.getElementById("main");
+
+    mainframe.appendChild(gameover_str);
+    mainframe.appendChild(scoretext);
+    context.restore();
 }
